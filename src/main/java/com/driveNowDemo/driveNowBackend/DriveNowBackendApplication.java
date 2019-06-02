763e9e9993ff38556cd33ad1ee4b8d304fe7eff1
emredennis <emredenizozer@gmail.com>
2019-06-02 19:55:34 +0200

    Sample project created.

---
+++ src/main/java/com/driveNowDemo/driveNowBackend/DriveNowBackendApplication.java
@@ -0,0 +1,13 @@
+package com.driveNowDemo.driveNowBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriveNowBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DriveNowBackendApplication.class, args);
+	}
+
+}
