519655a8718f6cbd91c6a327df065d2df9a29afd
Emre Deniz Özer <e.oezer@reply.de>
2019-06-03 19:42:07 +0200

    Class and package renaming in the project.

--- src/main/java/com/publicCarRentalDemo/publicCarRentalBackend/PublicCarRentalBackendApplication.java
+++ src/main/java/com/publicCarRentalDemo/publicCarRentalBackend/PublicCarRentalBackendApplication.java
@@ -1,13 +1,13 @@
-package com.driveNowDemo.driveNowBackend;
+package com.publicCarRentalDemo.publicCarRentalBackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DriveNowBackendApplication {
+public class PublicCarRentalBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DriveNowBackendApplication.class, args);
+		SpringApplication.run(PublicCarRentalBackendApplication.class, args);
 	}
 
 }
--- src/test/java/com/publicCarRentalDemo/publicCarRentalBackend/PublicCarRentalBackendApplicationTests.java
+++ src/test/java/com/publicCarRentalDemo/publicCarRentalBackend/PublicCarRentalBackendApplicationTests.java
@@ -1,4 +1,4 @@
-package com.driveNowDemo.driveNowBackend;
+package com.publicCarRentalDemo.publicCarRentalBackend;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DriveNowBackendApplicationTests {
+public class PublicCarRentalBackendApplicationTests {
 
 	@Test
 	public void contextLoads() {
