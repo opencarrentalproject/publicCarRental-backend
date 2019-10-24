210b3074f387e972d713d6da89f5c91bebd3afbc
m.guersul <m.guersul@reply.de>
2019-10-24 18:26:22 +0200

    @Configuration classess were refactored

--- src/main/java/com/publicCarRentalDemo/publicCarRentalBackend/LocaleConfig.java
+++ src/main/java/com/publicCarRentalDemo/publicCarRentalBackend/LocaleConfig.java
@@ -7,27 +7,12 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Locale;
 
 @Configuration
-@EnableSwagger2
-public class AppConfig implements WebMvcConfigurer{
-    @Bean
-    public Docket api() { 
-        return new Docket(DocumentationType.SWAGGER_2)  
-          .select()                                  
-          .apis(RequestHandlerSelectors.any())              
-          .paths(PathSelectors.any())                          
-          .build();                                           
-    }
-
-    @Bean
+public class LocaleConfig implements WebMvcConfigurer{
+     @Bean
     public LocaleResolver localeResolver() {
         SessionLocaleResolver slr = new SessionLocaleResolver();
         slr.setDefaultLocale(Locale.US);
